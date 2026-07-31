bbbcb51c591ab150671f04c1087331519c16b546
TK87code <t.kimura0311@gmail.com>
2026-07-31 14:00:23 +0900

    drew basic map, character, cursor

--- main.c
+++ main.c
@@ -20,6 +20,10 @@ struct map_cell {
 
 struct game_state {
 	struct map_cell map[MAP_ROW][MAP_COL];
+	int player_x;
+	int player_y;
+	int cursor_x;
+	int cursor_y;
 };
 
 void game_init(void *user_data) 
@@ -29,11 +33,37 @@ void game_init(void *user_data)
 	for (int y = 0; y < MAP_ROW; y++) {
 		for (int x = 0; x < MAP_COL; x++) {
 			state->map[y][x].type = TERRAIN_SEA;
+			state->map[y][x].is_walkable = 0;
 			state->map[y][x].fcolor = PKT_COLOR_BLUE;
 			state->map[y][x].bcolor = PKT_COLOR_BLACK;
 			state->map[y][x].symbol = '~';
 		}
 	}
+
+	for (int y = 4; y < 20; y++) {
+		for (int x = 10; x < 70; x++) {
+			state->map[y][x].type = TERRAIN_SEA;
+			state->map[y][x].is_walkable = 1;
+			state->map[y][x].fcolor = PKT_COLOR_YELLOW;
+			state->map[y][x].bcolor = PKT_COLOR_BLACK;
+			state->map[y][x].symbol = ':';
+		}
+	}
+
+	for (int y = 6; y < 18; y++) {
+		for (int x = 15; x < 65; x++) {
+			state->map[y][x].type = TERRAIN_SEA;
+			state->map[y][x].is_walkable = 1;
+			state->map[y][x].fcolor = PKT_COLOR_GREEN;
+			state->map[y][x].bcolor = PKT_COLOR_BLACK;
+			state->map[y][x].symbol = ',';
+		}
+	}
+
+	state->player_x = 40;
+	state->player_y = 12; 
+	state->cursor_x = 41;
+	state->cursor_y = 12;
 }
 
 void game_update(void *user_data, float dt)
@@ -54,11 +84,15 @@ void game_update(void *user_data, float dt)
 void game_draw(void *user_data)
 {
 	struct game_state *state = (struct game_state *)user_data;
+	
 	for (int y = 0; y < MAP_ROW; y++) {
 		for (int x = 0; x < MAP_COL; x++) {
 			pkt_putc(x, y, state->map[y][x].fcolor, state->map[y][x].bcolor, state->map[y][x].symbol);
 		}
 	}
+
+	pkt_puts(state->player_x, state->player_y, PKT_COLOR_WHITE, PKT_COLOR_BLACK, "☺");
+	pkt_putc(state->cursor_x, state->cursor_y, PKT_COLOR_YELLOW, PKT_COLOR_BLACK, 'X');
 }
 
 int main(void) 
